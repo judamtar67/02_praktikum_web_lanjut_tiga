8635742dcf9543eef774502375c04b5cff05a9db
judamtar67 <judamalmukhtar72@gmail.com>
2022-03-05 10:05:33 +0700

    Kode program 2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', 'https://www.educastudio.com/');
+
+Route::prefix('category')->group(function () {
+    Route::redirect('/edu-games', 'https://www.educastudio.com/category/marbel-edu-games');
+    Route::redirect('/kids-games', 'https://www.educastudio.com/category/marbel-and-friends-kids-games');
+    Route::redirect('/story-books', 'https://www.educastudio.com/category/riri-story-books');
+    Route::redirect('/kids-songs', 'https://www.educastudio.com/category/kolak-kids-songs');
 });
+
+Route::get('/news/{id}', function ($id=1) {
+    return redirect()->route('News');
+});
+Route::get('/news/{id}', function ($id=2) {
+    return redirect()->route('News2');
+});
+
+Route::redirect('/', 'https://www.educastudio.com/news')->name('News');
+Route::redirect('/', 'https://www.educastudio.com/news/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19')
+->name('News2');
+
+Route::prefix('program')->group(function () {
+    Route::redirect('/karir', 'https://www.educastudio.com/program/karir');
+    Route::redirect('/magang', 'https://www.educastudio.com/program/magang');
+    Route::redirect('/kunjungan', 'https://www.educastudio.com/program/kunjungan-industri');
+});
+
+Route::redirect('about-us', 'https://www.educastudio.com/about-us');
+
+Route::resource('Contact', App\Htttp\Controllers\ContactController::class)->only([
+    'index'
+]);
